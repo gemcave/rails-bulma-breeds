be93df7f990e95db393cb411107e95d4650c36e1
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-11-21 14:39:47 +0600

    added an attached image for horses

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Horse do
-  permit_params :name, :breed_id, :age, :number_of_legs, :top_speed  
+	permit_params :name, :breed_id, :age, :number_of_legs, :top_speed, :image
+
+	form do |f|
+		f.semantic_errors # shows errors on :base
+		f.inputs          # builds an input field for every attribute
+		f.inputs do          # builds an input field for every attribute
+			f.input :image, as: :file
+		end
+		f.actions         # adds the 'Submit' and 'Cancel' buttons
+	end
 end
--- app/models/horse.rb
+++ app/models/horse.rb
@@ -1,5 +1,7 @@
 class Horse < ApplicationRecord
   belongs_to :breed
 
-  validates :name, presence: true
+	validates :name, presence: true
+	
+	has_one_attached :image
 end
